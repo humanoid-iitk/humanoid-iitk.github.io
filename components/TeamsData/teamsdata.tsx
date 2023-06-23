acddb4fef887b1a833e52a6ebe774c488f562874
AshNewar <jnvspr12345@gmail.com>
2023-06-23 16:11:21 +0530

    Teams page Added

---
+++ components/TeamsData/teamsdata.tsx
@@ -0,0 +1,21 @@
+import student from "../../public/images/teams/student.jpg";
+
+const TeamHead = [
+  { name: "Aadvik Kumar", photo:student },
+  { name: "Khushi Agarwal", photo:student },
+  { name: "Ankit Kumar", photo:student },
+  { name: "Siddharth Garg", photo: student},
+  { name: "Mehar Goenka", photo:student },
+];
+
+const TeamMembers = [
+  { name: "Divyansh Mittal", photo:student },
+  { name: "Krishnansh Agarwal", photo:student },
+  { name: "Shaurya Singh", photo:student },
+  { name: "Om Shrivastava", photo:student },
+  { name: "Aayushman Gupta", photo:student },
+  { name: "Jatin Bansal", photo:student },
+  { name: "Shivanshu Thele", photo:student },
+];
+
+export { TeamHead, TeamMembers };
