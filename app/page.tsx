1afd5d88dc0c9843a133134d03c4cfe57770f841
Divyansh Mittal <dmittal21@iitk.ac.in>
2023-07-13 17:05:54 +0530

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import { Inter } from "@next/font/google";
-import Achievement from "./achievment/page";
-import Humanoid from "./humanoids/page";
-import SponsorPage from "./sponsors/page";
-import TeamPage from "./teams/page";
-import AboutPage from "./about/page";
+import Achievement from "@/app/achievment/page";
+import Humanoid from "@/app/humanoids/page";
+import SponsorPage from "@/app/sponsors/page";
+import TeamPage from "@/app/teams/page";
+import AboutPage from "@/app/about/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
